2716577b52ef369e49446c4ebd24f60fb01a988c
Mariano Tuero <mariano.tuero@gmail.com>
2025-08-26 23:01:20 +0200

    initial commit fastAPI PoC

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI # type: ignore
+from pydantic import BaseModel # type: ignore
 
 app = FastAPI()
 
 # Endpoint raíz
 @app.get("/")
 def read_root():
-    return {"mensaje": "Hola mundo desde el microservicio Dockerizado"}
+    return {"mensaje": "Hello World – a Dockerized FastAPI microservice with a Python 3 backend."}
 
 # Endpoint dinámico con path parameter
-@app.get("/saludo/{nombre}")
+@app.get("/Hi/{nombre}")
 def saludo(nombre: str):
-    return {"mensaje": f"Hola, {nombre}! Bienvenido a mi microservicio."}
+    return {"mensaje": f"Hola, {nombre}! Welcome to my microservice."}
 
 # Endpoint con query parameters opcionales
 @app.get("/info")
 def info(nombre: str = "Mariano", edad: int = 45):
-    return {"nombre": nombre, "edad": edad, "mensaje": f"Hola {nombre}, tenés {edad} años"}
+    return {"name": nombre, "Age": edad, "message": f"Hi {nombre}, you are {edad} years old"}
 
 # Modelo Pydantic para POST
 class Mensaje(BaseModel):
     texto: str
 
 # Endpoint POST para procesar texto
-@app.post("/procesar")
+@app.post("/convert")
 def procesar(mensaje: Mensaje):
     return {
         "original": mensaje.texto,
